b8bc73db053bfc7e9e5d022c99a482e19af6204f
Victor Aigbeghian <pfmreal@gmail.com>
2022-01-06 15:33:03 +0100

    Updated the upgrade classes.

--- src/Admin/Upgrades/Prayed_Counts.php
+++ src/Admin/Upgrades/Prayed_Counts.php
@@ -1,59 +1,63 @@
 <?php
 /**
- * 3.0 Data Migration - Order Notes.
+ * Intercessor Prayed Counts Upgrade
  *
- * @subpackage  Admin/Upgrades/v3
- * @copyright   Copyright (c) 2018, Easy Digital Downloads, LLC
- * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
- * @since       3.0
+ * @subpackage  Admin/Upgrades/Prayed_Counts
+ * @copyright   Copyright (c) 2021, Victor Aigbeghian
+ * @license     http://opensource.org/licenses/gpl-3.0.php GNU Public License
+ * @since       1.0.0
  */
-namespace EDD\Admin\Upgrades\v3;
 
-// Exit if accessed directly
+namespace Intercessor\Admin\Upgrades;
+
+// Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
 /**
- * Order_Notes Class.
+ * Prayed_Counts Update Class.
  *
- * @since 3.0
+ * @since 1.1.0
  */
-class Order_Notes extends Base {
+class Prayed_Counts extends Base {
 
 	/**
 	 * Constructor.
 	 *
 	 * @param int $step Step.
+     *
+     * @access public
+     * @since 1.1.0
 	 */
 	public function __construct( $step = 1 ) {
 		parent::__construct( $step );
 
-		$this->completed_message = __( 'Order notes migration completed successfully.', 'easy-digital-downloads' );
-		$this->upgrade           = 'migrate_order_notes';
+		$this->completed_message = esc_html__( 'Prayed counts migration completed successfully.', 'intercessor' );
+		$this->upgrade           = 'prayed_counts';
 	}
 
 	/**
 	 * Retrieve the data pertaining to the current step and migrate as necessary.
 	 *
-	 * @since 3.0
+	 * @since 1.1.0
 	 *
 	 * @return bool True if data was migrated, false otherwise.
 	 */
-	public function get_data() {
+	public function get_data() : bool {
 		$offset = ( $this->step - 1 ) * $this->per_step;
 
 		$results = $this->get_db()->get_results( $this->get_db()->prepare(
 			"SELECT *
-			 FROM {$this->get_db()->comments}
-			 WHERE comment_type = %s
-			 ORDER BY comment_id ASC
+			 FROM {$this->get_db()->ipr_prayer_meta}
+			 WHERE meta_key = %s
+			 ORDER BY ipr_prayer_id ASC
 			 LIMIT %d, %d",
-			esc_sql( 'edd_payment_note' ), $offset, $this->per_step
+			esc_sql( 'prayed_counts' ), $offset, $this->per_step
 		) );
 
 		if ( ! empty( $results ) ) {
 			foreach ( $results as $result ) {
 				$result->object_id = $result->comment_post_ID;
-				Data_Migrator::order_notes( $result );
+				Migrator::prayed_counts( $result );
 			}
 
 			return true;
@@ -65,13 +69,14 @@ class Order_Notes extends Base {
 	/**
 	 * Calculate the percentage completed.
 	 *
-	 * @since 3.0
+	 * @since 1.1.0
 	 *
 	 * @return float Percentage.
 	 */
 	public function get_percentage_complete() {
-		$total = $this->get_db()->get_var( $this->get_db()->prepare( "SELECT COUNT(comment_ID) AS count FROM {$this->get_db()->comments} WHERE comment_type = %s", esc_sql( 'edd_payment_note' ) ) );
+		$total = $this->get_db()->get_var( $this->get_db()->prepare( "SELECT COUNT(ipr_prayer_id) AS count FROM {$this->get_db()->ipr_prayer_meta} WHERE meta_key = %s", esc_sql( 'prayed_counts' ) ) );
 
+        // Set counter to null if nothing exists in the database.
 		if ( empty( $total ) ) {
 			$total = 0;
 		}
